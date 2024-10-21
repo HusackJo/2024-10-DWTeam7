718e206160459cead0dced391cc11282e1d1ac2b
TBrown211 <150560857+TBrown211@users.noreply.github.com>
2024-10-21 12:05:49 -0400

    Changing some things

--- DWTEAM7/Assets/Scripts/Move_Ground.cs
+++ DWTEAM7/Assets/Scripts/Move_Ground.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move_Platform : MonoBehaviour
+public class Move_Ground : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()

b7ffde18b7a11dcf3564466a839aa0e9a15d03ee
TBrown211 <150560857+TBrown211@users.noreply.github.com>
2024-10-21 13:28:30 -0400

    Added variables to script

--- DWTEAM7/Assets/Scripts/Move_Ground.cs
+++ DWTEAM7/Assets/Scripts/Move_Ground.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Move_Ground : MonoBehaviour
 {
+    [SerializeField]
+    private float scrollFactor = 2.0f;
+    [SerializeField]
+    private float scrollSpeed;
+    [SerializeField]
+    private float paralaxEffect;
+
+    private SpriteRenderer sr;
+    private float length;
+    private float count;
+    [SerializeField]
+    private string objTag;
+
     // Start is called before the first frame update
     void Start()
     {

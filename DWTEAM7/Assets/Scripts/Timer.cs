acbde774db28b7f022ccd7261b6354c87fafbb14
TBrown211 <150560857+TBrown211@users.noreply.github.com>
2024-10-22 16:35:00 -0400

    Getting started on the timer

--- DWTEAM7/Assets/Scripts/Timer.cs
+++ DWTEAM7/Assets/Scripts/Timer.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
+    [SerializeField]
+    private float timeCounter;
+    [SerializeField]
+    private int seconds;
+    [SerializeField]
+    private int minutes;
+    [SerializeField]
+    private TextMeshProUGUI timerText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,8 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timeCounter += Time.deltaTime;
+        minutes = Mathf.FloorToInt(timeCounter / 60f);
+        seconds = Mathf.FloorToInt(timeCounter - minutes *60);
     }
 }

9a4893f4350ed4308d49c9adad272aa8cc44ea91
TBrown211 <150560857+TBrown211@users.noreply.github.com>
2024-10-22 12:12:44 -0400

    New Ground Spawner Added

--- DWTEAM7/Assets/Scripts/Spawn_Ground.cs
+++ DWTEAM7/Assets/Scripts/Spawn_Ground.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Spawn_Ground : MonoBehaviour
 {
+    public GameObject ground;
+
+    public float spawnInterval;
+    private float spawnTime;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnTime = Time.time + spawnInterval;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time >= spawnTime)
+        {
+            SpawnGround();
+            spawnTime = Time.time +spawnInterval;
+        }
+    }
+
+    void SpawnGround()
+    {
+        GameObject newGround = Instantiate(ground);
+
+        newGround.transform.position = new Vector2 (22, -4);
+
+        newGround.name = "New Ground";
     }
 }

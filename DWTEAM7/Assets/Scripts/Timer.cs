a57b946a514db359742c43453aa7272dd41630e5
TBrown211 <150560857+TBrown211@users.noreply.github.com>
2024-10-23 15:57:11 -0400

    Best Timer is working

--- DWTEAM7/Assets/Scripts/Timer.cs
+++ DWTEAM7/Assets/Scripts/Timer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
@@ -13,12 +14,25 @@ public class Timer : MonoBehaviour
     private int minutes;
     [SerializeField]
     private TextMeshProUGUI timerText;
+    [SerializeField]
+    private TextMeshProUGUI bestTime;
 
+    private void Start()
+    {
+        bestTime.text = PlayerPrefs.GetString("Best Time", "00:00");
+    }
     void Update()
     {
         timeCounter += Time.deltaTime;
         minutes = Mathf.FloorToInt(timeCounter / 60f);
         seconds = Mathf.FloorToInt(timeCounter - minutes *60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        if (timerText.text != PlayerPrefs.GetString("Best Time", "00:00"))
+        {
+            PlayerPrefs.SetString("Best Time", timerText.text);
+        }
+        
     }
+
 }
